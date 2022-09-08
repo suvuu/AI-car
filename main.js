4bcae1c32f01fc2f911578593721e8ab9526919a
suvuu <hshakim597@gmail.com>
2022-09-08 13:17:53 -0400

    Self driving car

---
+++ main.js
@@ -0,0 +1,100 @@
+const carCanvas = document.getElementById("carCanvas");
+carCanvas.width=200;
+
+
+const carCtx = carCanvas.getContext("2d");
+
+
+const road=new Road(carCanvas.width/2,carCanvas.width*0.9);
+
+
+
+const N=100;
+const cars=generateCars(N);
+let bestCar=cars[0];
+if(localStorage.getItem("bestBrain")){
+    for(let i=0;i<cars.length;i++){
+cars[i].brain=JSON.parse(
+        localStorage.getItem("bestBrain"));
+        if(i!=0){
+            NeuralNetwork.mutate(cars[i].brain,.1);
+    }
+}
+}
+
+
+
+const traffic=[
+    new Car(road.getLaneCenter(1),-100,30,50,"DUMMY", 2),
+    new Car(road.getLaneCenter(0),-300,30,50,"DUMMY", 2),
+    new Car(road.getLaneCenter(2),-300,30,50,"DUMMY", 2),
+    new Car(road.getLaneCenter(0),-500,30,50,"DUMMY", 2),
+    new Car(road.getLaneCenter(1),-500,30,50,"DUMMY", 2),
+    new Car(road.getLaneCenter(0),-600,30,50,"DUMMY", 2),
+    new Car(road.getLaneCenter(2),-6940,30,50,"DUMMY", 2),
+    new Car(road.getLaneCenter(2),-700,30,50,"DUMMY", 2),
+    new Car(road.getLaneCenter(1),-850,30,50,"DUMMY", 2),
+    new Car(road.getLaneCenter(1),-930,30,50,"DUMMY", 2),
+    new Car(road.getLaneCenter(0),-1000,30,50,"DUMMY", 2),
+    new Car(road.getLaneCenter(2),-1100,30,50,"DUMMY", 2),
+
+]
+
+
+animate();
+
+function save(){
+    localStorage.setItem("bestBrain",
+    JSON.stringify(bestCar.brain))
+}
+
+function generateCars(){
+    const cars = [];
+    for(let i=1;i<=N;i++){
+        cars.push(new Car(road.getLaneCenter(1),100,30,50,"AI"));
+
+    }
+    return cars;
+}
+
+
+function animate(){  
+    for(let i=0;i<traffic.length;i++){
+        traffic[i].update(road.borders,[]);
+    }
+    
+    for(let i=0;i<cars.length;i++){
+        cars[i].update(road.borders,traffic);
+    }
+
+    // find car that has min y bestcar
+    bestCar=cars.find(
+        c=>c.y==Math.min(
+            ...cars.map(c=>c.y)
+    ));
+    
+
+    carCanvas.height=window.innerHeight;
+  
+
+    carCtx.save();
+
+    //camera follows best car
+    carCtx.translate(0,-bestCar.y+carCanvas.height*0.7);
+  
+    road.draw(carCtx);
+    for(let i=0;i<traffic.length;i++){
+        traffic[i].draw(carCtx,"purple");
+    }
+    carCtx.globalAlpha=.2;
+    for(let i=0;i<cars.length;i++){
+        cars[i].draw(carCtx,"green");
+    }
+    carCtx.globalAlpha=1;
+    bestCar.draw(carCtx,"black",true);
+
+    carCtx.restore();
+
+    
+    requestAnimationFrame(animate);
+}
